70ff8490a402deb37ebcf748e5fadc137e76ddc7
Neil Booth <kyuupichan@gmail.com>
2018-01-06 17:02:09 +0800

    Initial revision

---
+++ client/app.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2018, Neil Booth
+#
+# All rights reserved.
+#
+# See the file "LICENCE" for information about the copyright
+# and warranty status of this software.
+
+class App(object):
+
+    NAME = 'Electron Cash'
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
